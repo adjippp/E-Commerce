74762b2479505fcb3454728b61f9bc21bda375c2
Adji Pratama Permana Putra <42745069+adjippp@users.noreply.github.com>
2020-01-18 10:53:23 +0700

    Delete Pembeli.java

--- src/main/java/com/doku/da/dokumart/audadokumart/model/Pembeli.java
+++
@@ -1,7 +0,0 @@
-package com.doku.da.dokumart.audadokumart.model;
-
-import com.doku.da.dokumart.audadokumart.interfaces.Client;
-
-public class Pembeli implements Client {
-    Keranjang keranjang = new Keranjang();
-}
